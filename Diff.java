9380d16df3561f71d181103aebb75205567dad36
rokesh1 <rokesh2000@gmail.com>
2023-04-03 20:32:40 +0530

    two aded diff

---
+++ Diff.java
@@ -0,0 +1,10 @@
+public class Diff {
+
+    public static void main(String args[])   
+{  
+int n1 = 10, n2 = 90, dif;  
+dif = n1 - n2;  
+System.out.println("The dif of numbers is: "+dif);  
+}  
+    
+}

c78da790c2e42edfb46f2bcde1bb437337517e31
rokesh1 <rokesh2000@gmail.com>
2023-04-03 20:58:16 +0530

    three aded mul

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+
+    public static void main(String args[])   
+{  
+int n1 = 10, n2 = 90, pro;  
+pro = n1 * n2;  
+System.out.println("The Product of numbers is: "+pro);  
+} 
+    
+}
